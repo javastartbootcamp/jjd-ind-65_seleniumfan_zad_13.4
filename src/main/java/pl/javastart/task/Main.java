aa578adc769d4f3ef3fa157495fee4d984be828e
Tomasz Iwanicki <tiko35@o2.pl>
2023-04-21 00:10:39 +0200

    solution

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,5 +1,8 @@
 package pl.javastart.task;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,7 +11,13 @@ public class Main {
 
         // lista powinna być parametryzowana (analogicznie jak java.util.ArrayList czy java.util.LinkedList)
         CustomList<String> customListOfStrings = new CustomList();
-        CustomList<Integer> customListOfIntegers = new CustomList();
-
+        customListOfStrings.add("Tomasz");
+        customListOfStrings.add("Mateuszek");
+        customListOfStrings.add("Przemek");
+        customListOfStrings.add("Tadek");
+        customListOfStrings.add("Wiola");
+        customListOfStrings.add("Angelika");
+        customListOfStrings.add(3, "Test");
+        System.out.println(customListOfStrings);
     }
 }
